16b4951afbbf9eee40ed818c7f035872adfba0ff
Martin Achtermann <65299501+roadpilot@users.noreply.github.com>
2021-09-06 01:46:36 +0000

    rough 1

--- index.js
+++ index.js
@@ -3,11 +3,31 @@
 
 <head>
 <meta content="text/html; charset=utf-8" http-equiv="Content-Type" />
-<title>Untitled 1</title>
+<title>Self expanding textarea</title>
+<style>
+.autoexpand{
+  resize:none;
+	overflow-y: hidden;
+	overflow-x: hidden;
+}
+</style>
 </head>
 
 <body>
+<script>
+function autoExpand(elem) {
+  elem.style.height = 'auto';
+  elem.style.height = (elem.scrollHeight-4) + 'px';
+}
+function keyPressHandler(elem,e) {
+      console.log(window.event.shiftKey)
+      var kC  = (window.event) ?    // MSIE or Firefox?
+                 event.keyCode : e.keyCode;
+      if(kC==13 && !e.shiftKey){elem.form.submit()}
+}
 
+</script>
+<textarea class="autoexpand" onkeypress="keyPressHandler(this)" oninput="autoExpand(this)" rows="1" maxlength="255" name="field0" style="width_:50px;"></textarea>
 </body>
 
 </html>
